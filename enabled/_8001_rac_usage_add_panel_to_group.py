000cb54a6b18b40f93f035d967be880a04c1c111
Cybera Sysadmin <sysadmin@cybera.ca>
2017-06-24 22:23:38 -0600

    Adding RAC Usage dashboard

---
+++ enabled/_8001_rac_usage_add_panel_to_group.py
@@ -0,0 +1,9 @@
+PANEL = 'Usage Graphs'
+PANEL_DASHBOARD = 'project'
+PANEL_GROUP = 'rac'
+
+ADD_PANEL = 'nectar_dashboard.rac_usage.panel.RACUsage'
+
+ADD_INSTALLED_APPS = [
+    'nectar_dashboard.rac_usage',
+]
