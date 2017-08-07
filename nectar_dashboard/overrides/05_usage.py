fab9045b4a120d56ce0268279207c2864413e392
Cybera Sysadmin <sysadmin@cybera.ca>
2017-08-07 12:16:07 -0600

    Disabling csv link

--- nectar_dashboard/overrides/05_usage.py
+++ nectar_dashboard/overrides/05_usage.py
@@ -1,10 +1,45 @@
+from django.core import urlresolvers
+from django.utils.translation import ugettext_lazy as _
+from horizon import tables
+from horizon.utils import filters
 from openstack_dashboard import usage
 from openstack_dashboard.dashboards.project.overview import views
 
 from nectar_dashboard import helpers
 
+# This is a local copy from openstack_dashboard.usage.
+# Horizon breaks if this isn't here.
+def get_instance_link(datum):
+    view = "horizon:project:instances:detail"
+    if datum.get('instance_id', False):
+        return urlresolvers.reverse(view, args=(datum.get('instance_id'),))
+    else:
+        return None
+
+# This overrides usage.ProjectUsage table to disable the CSV and Juju links
+class CyberaProjectUsageTable(usage.BaseUsageTable):
+    instance = tables.Column('name',
+                             verbose_name=_("Instance Name"),
+                             link=get_instance_link)
+    uptime = tables.Column('uptime_at',
+                           verbose_name=_("Time since created"),
+                           filters=(filters.timesince_sortable,),
+                           attrs={'data-type': 'timesince'})
+
+    def get_object_id(self, datum):
+        return datum.get('instance_id', id(datum))
+
+    class Meta(object):
+        name = "project_usage"
+        hidden_title = False
+        verbose_name = _("Usage")
+        columns = ("instance", "vcpus", "disk", "memory", "uptime")
+        table_actions = ()
+        multi_select = False
+
+# This enables our custom usage/quota calulcation to appear on the overview page.
 class CyberaProjectOverview(views.ProjectOverview):
-    table_class = usage.ProjectUsageTable
+    table_class = CyberaProjectUsageTable
     usage_class = usage.ProjectUsage
     template_name = 'project/overview/usage.html'
     csv_response_class = views.ProjectUsageCsvRenderer
@@ -21,4 +56,5 @@ class CyberaProjectOverview(views.ProjectOverview):
             return []
 	return self.usage.get_instances()
 
+usage.ProjectUsageTable = CyberaProjectUsageTable
 views.ProjectOverview = CyberaProjectOverview
