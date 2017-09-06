a83b44a853112601a85e2eb77cd5918cb461bdde
Cameron Mann <cameron.mann@cybera.ca>
2017-09-06 14:27:35 -0600

    Fix indents and delete forms.py

--- nectar_dashboard/fwaas/panel.py
+++ nectar_dashboard/fwaas/panel.py
@@ -3,13 +3,13 @@ from django.utils.translation import ugettext_lazy as _
 import horizon
 
 class Fwaas(horizon.Panel):
-	name = _("FWaaS")
-	slug = 'fwaas'
+    name = _("FWaaS")
+    slug = 'fwaas'
 
-	def allowed(self, context):
-            return True
-            request = context['request']
-            for role in request.user.roles:
-                    if role['name'] == 'FWaaS':
-                            return True
-                    return False
+    def allowed(self, context):
+        return True
+        request = context['request']
+        for role in request.user.roles:
+            if role['name'] == 'FWaaS':
+                return True
+        return False
