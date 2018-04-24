f9064d241668897e64d6d4ea749277bc7b57706f
Cameron Mann <cameron.mann@cybera.ca>
2018-04-24 20:47:21 +0000

    Only if sahara is enabled

--- nectar_dashboard/overrides/09_restrict_sahara.py
+++ nectar_dashboard/overrides/09_restrict_sahara.py
@@ -4,5 +4,6 @@ import horizon
 
 project = horizon.get_dashboard("project")
 data_processing = project.get_panel_group("data_processing")
-for i in data_processing:
-    i.permissions = ('openstack.roles.sahara_user',)
+if data_processing is not None:
+    for i in data_processing:
+        i.permissions = ('openstack.roles.sahara_user',)
