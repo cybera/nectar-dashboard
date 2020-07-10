323ca6cbdd664fabb1bd31ea62fe34b421ad3465
Cybera Sysadmin <sysadmin@cybera.ca>
2020-07-10 02:36:31 +0000

    Add note about IPv4 versus IPv6 to form to help users know how they are connecting

--- nectar_dashboard/rac_quota_change/views.py
+++ nectar_dashboard/rac_quota_change/views.py
@@ -7,6 +7,7 @@ from .forms import RACQuotaChangeForm
 import logging
 LOG = logging.getLogger(__name__)
 
+from netaddr import *
 
 class RACQuotaChangeView(forms.ModalFormView):
     form_class = RACQuotaChangeForm
@@ -15,6 +16,7 @@ class RACQuotaChangeView(forms.ModalFormView):
     def get_context_data(self, **kwargs):
         context = super(RACQuotaChangeView, self).get_context_data(**kwargs)
         context['current_quota'] = quotas.tenant_quota_usages(self.request)
+        context['remote_addr'] = IPAddress(self.request.META.get('HTTP_X_FORWARDED_FOR')).version
         return context
 
     def get_success_url(self):
